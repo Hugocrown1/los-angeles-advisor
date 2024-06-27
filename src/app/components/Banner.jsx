8044543bd5540fa9cccb1a3ce79c96e69af104c4
Hugocrown1 <hugo.corona.r@gmail.com>
2024-06-27 16:31:32 -0700

    Refactor Banner component

--- src/app/components/Banner.jsx
+++ src/app/components/Banner.jsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import { BIZ_UDPGothic } from "next/font/google";
+
+const gothic = BIZ_UDPGothic({ subsets: ["latin"], weight: "400" });
 
 const Banner = () => {
   return (
     <section>
-      <div className="flex h-[176px] items-center justify-between w-[1620px] mx-auto">
+      <div className="flex h-[176px] items-center justify-between w-[1500px] mx-auto">
         <figure className="relative w-[295px] h-[116px] bg-black rounded-xl overflow-hidden">
           <Image
             src={"/assets/LA-downtown-banner.webp"}
@@ -13,8 +16,16 @@ const Banner = () => {
             className="object-cover object-center"
           />
         </figure>
-        <div className="relative flex w-full">
-          <span className="text-[#EDAE49] absolute text-[400px]">"</span>
+
+        <div className="flex relative items-center justify-start  w-[50%] h-4 ">
+          <span
+            className={`${gothic.className} text-[#edae4974] absolute text-[360px] -translate-x-8 translate-y-24 -z-10`}
+          >
+            "
+          </span>
+          <p className="font-semibold italic text-[48px]">
+            I am an angel, this is the city where I belong
+          </p>
         </div>
       </div>
     </section>
