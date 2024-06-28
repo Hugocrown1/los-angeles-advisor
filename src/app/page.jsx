8fb0cc1f0e1b81d32698f94851b5d3cbe8e64edc
Hugocrown1 <hugo.corona.r@gmail.com>
2024-06-28 14:54:38 -0700

    Added new sections to main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Banner from "../components/Banner";
 import Hero from "../components/Hero";
 import Attractions from "@/components/Attractions";
+import CategoriesDisplay from "@/components/CategoriesDisplay";
+import AboutUs from "@/components/AboutUs";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <Banner />
       <Hero />
       <Attractions />
+      <CategoriesDisplay />
+      <AboutUs />
     </main>
   );
 }
