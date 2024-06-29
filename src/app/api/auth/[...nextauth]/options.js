2752eab1e7916242983406cc2402447f6f13f53a
Hugocrown1 <hugo.corona.r@gmail.com>
2024-06-29 14:00:23 -0700

    Added authentication API routes and user model

---
+++ src/app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,58 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+import User from "@/models/User";
+export const options = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "@ejemplo.com" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "********",
+        },
+      },
+      async authorize(credentials, req) {
+        try {
+          const foundUser = await User.findOne({ email: credentials.email })
+            .lean()
+            .exec();
+
+          if (foundUser) {
+            const match = await bcrypt.compare(
+              credentials.password,
+              foundUser.password
+            );
+
+            if (match) {
+              delete foundUser.password;
+
+              if (foundUser.email === "jsonsmith@json.com") {
+                foundUser["role"] = "admin";
+                return foundUser;
+              }
+
+              foundUser["role"] = "user";
+              return foundUser;
+            }
+          }
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+
+      return session;
+    },
+  },
+};
---
+++ src/app/api/users/route.js
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import User from "@/models/User";
+import bcrypt from "bcrypt";
+
+export async function POST(request) {
+  try {
+    const body = await request.json();
+
+    const userData = body;
+
+    if (!userData.name || !userData.email || !userData.password) {
+      return NextResponse.json([
+        { message: "Faltan campos por llenar" },
+        { status: 400 },
+      ]);
+    }
+
+    // Verificar correos duplicados
+    const duplicate = await User.findOne({ email: userData.email })
+      .lean()
+      .exec();
+
+    if (duplicate) {
+      return NextResponse.json(
+        { message: "El correo ya esta registrado" },
+        { status: 409 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(userData.password, 10);
+    userData.password = hashedPassword;
+
+    await User.create(userData);
+    return NextResponse.json(
+      { message: "Usuario creado exitosamente" },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.error(error);
+  }
+}
