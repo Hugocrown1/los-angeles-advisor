2cb92d95a574d4d72a51cd9ed0195e6fb2089c76
Hugocrown1 <hugo.corona.r@gmail.com>
2024-06-29 14:01:48 -0700

    Add denegado and only-admin pages

---
+++ src/app/denegado/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <main>
+      <span className="text-xl font-bold text-red-600">Acceso denegado</span>
+    </main>
+  );
+};
+
+export default page;
---
+++ src/app/only-admin/page.jsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <main>
+      <span className="text-xl font-bold text-green-600">
+        Acceso concedido!
+      </span>
+    </main>
+  );
+};
+
+export default page;
