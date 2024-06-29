122bd9ef46b90f82ff39201cdee53f869abb042e
Hugocrown1 <hugo.corona.r@gmail.com>
2024-06-28 17:00:24 -0700

    Refactor Header component and update site title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="flex justify-between items-center w-full min-[1255px]:w-[1200px] h-[60px] mx-auto text-[#ffff]">
         <Link className="flex justify-center items-center gap-x-2" href="#home">
           <Sunset height="48" stroke="#EB9D34" />
-          <h1>Los Angeles Advisor</h1>
+          <span>Los Angeles Advisor</span>
         </Link>
         <div className="flex flex-row items-center text-[18px]">
           <nav>
